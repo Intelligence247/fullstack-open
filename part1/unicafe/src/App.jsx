2b641513bdf3d86e2213504250bc36f704f8654b
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-02-22 23:04:00 +0100

    unicafe step5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,6 +7,16 @@ const App = () => {
   const all = good+neutral+bad
   const positive= (good*100)/all
   const average = (neutral+bad)/good
+const StatisticLine=(props)=>{
+  return(
+  <div style={{display:"flex", gap:".5rem"}}>
+  <p>{props.text}</p>
+  <p>{props.value}</p>
+  </div>
+  )
+}
+
+
 const Stattistics = (props) =>{
   if(all<=0){
     return 'No feedback given'
@@ -16,13 +25,20 @@ const Stattistics = (props) =>{
   
   return(
  <>
-  <h1>Statistic</h1>
+  {/* <h1>Statistic</h1>
 <p>Good {props.good}</p>
 <p>Neutral {props.neutral}</p>
 <p>Bad {props.bad}</p>
 <p>All {props.all}</p>
 <p>Average {props.average}</p>
-<p>Positive {props.positive} %</p>
+<p>Positive {props.positive} %</p> */}
+<StatisticLine text="good" value={props.good}/>
+<StatisticLine text="neutral" value={props.neutral}/>
+<StatisticLine text="bad" value={props.bad}/>
+<StatisticLine text="average" value={props.average}/>
+<StatisticLine text="positive" value={props.positive}/>
+
+
 </>
   )
 }

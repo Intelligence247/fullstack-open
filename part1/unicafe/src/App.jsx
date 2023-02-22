11c50ded0c4a79bd3ae15e619324169b4de68d4d
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-02-22 22:43:24 +0100

    unicafe step3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,20 +8,36 @@ const App = () => {
   const all = good+neutral+bad
   const positive= (good*100)/all
   const average = (neutral+bad)/good
+const Stattistics = (props) =>{
+  return(
+ <>
+  <h1>Statistic</h1>
+<p>Good {props.good}</p>
+<p>Neutral {props.neutral}</p>
+<p>Bad {props.bad}</p>
+<p>All {props.all}</p>
+<p>Average {props.average}</p>
+<p>Positive {props.positive} %</p>
+</>
+  )
+}
+
   return (
     <div>
        <h1 className='text-center text-[2rem] text-[green]'>Give Feedback</h1>
     <button onClick={()=>setGood(good+1)}>Good</button>
     <button onClick={()=>setNeutral(neutral+1)}>Neutral</button>
     <button onClick={()=>setBad(bad+1)}>Bad</button>
-      <div className="statistic">
-        <h1>Statistic</h1>
-    <p>Good {good}</p>
-    <p>Neutral {neutral}</p>
-    <p>Bad {bad}</p>
-    <p>All {all}</p>
-    <p>Average {average}</p>
-    <p>Positive {positive} %</p>
+    <div className="statistic">
+     <Stattistics
+     good={good}
+     neutral={neutral}
+     bad={bad}
+     all={all}
+     average={average}
+     positive={positive}
+
+     />
     </div>
     </div>
   )
